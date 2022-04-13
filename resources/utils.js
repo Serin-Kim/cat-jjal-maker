1962220ca99e77d7eeec9305a5214468929b9256
Serin-Kim <serin9864@gmail.com>
2022-04-13 17:47:25 +0900

    10 컴포넌트 만들기, 12 스타일링

--- resources/utils.js
+++ resources/utils.js
@@ -6,6 +6,9 @@ const CAT2 = 'https://cataas.com//cat/5e9970351b7a400011744233/says/inflearn';
 const CAT3 = 'https://cataas.com/cat/595f280b557291a9750ebf65/says/JavaScript';
 
 const HAMSTER1 = 'https://en.meming.world/images/en/5/5f/Scared_Hamster.jpg';
+const HAMSTER2 =
+  'https://i.pinimg.com/736x/f6/6a/f5/f66af5e07fed4fb306f5a6cc3c4ddf16.jpg';
+const HAMSTER3 = 'https://media.bunjang.co.kr/images/crop/635932315_w320.jpg';
 
 const includesHangul = (text) => /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/i.test(text);
 
